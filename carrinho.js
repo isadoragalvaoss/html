0c78758d973cd7f36c598f0eb08fa8e188a37d45
Isadora Soares <isadoragalvaoss@gmail.com>
2021-04-24 23:17:09 -0300

    Atualizando site responsivo

--- carrinho.js
+++ carrinho.js
@@ -7,3 +7,15 @@ function funcao2()
 {
 alert("Adicionado ao carrinho!");
 }
+
+const $menu = document.querySelector('.menu')
+const $botaoMenuOpen = document.querySelector('.botaoMenu_open')
+const $botaoMenuClose = document.querySelector('.botaoMenu_close')
+
+$botaoMenuOpen.addEventListener('click',function(){
+	$menu.classList.add('menu_open')
+})
+
+$botaoMenuClose.addEventListener('click',function(){
+	$menu.classList.remove('menu_open')
+})
